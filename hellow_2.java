ed71aa725ae557011b8c74414ec553e51bf78a1e
Hkm172Cse <azizulhakim000111@gmail.com>
2021-10-20 02:02:56 +0600

    print a String

--- hellow_2.java
+++ hellow_2.java
@@ -1,9 +1,13 @@
 public class hellow2{
     public static void main(String[] args){
         display("azizul hakim");
+
+        System.out.println("Visual Studio Code is Difficult Awosome");
     }
 
     public static void display(String name){
         System.out.println("my name is "+ name);
     }
+
+
 }
