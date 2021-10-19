24d61dc31effaf53bc26a3ac7eeddbd64dbf26aa
Hkm172Cse <azizulhakim000111@gmail.com>
2021-10-20 01:52:31 +0600

    added some line in helhow_2.java

---
+++ hellow_2.java
@@ -0,0 +1,9 @@
+public class hellow2{
+    public static void main(String[] args){
+        display("azizul hakim");
+    }
+
+    public static void display(String name){
+        System.out.println("my name is "+ name);
+    }
+}
