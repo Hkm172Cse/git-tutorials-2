d2333d9d0a193ef52a718166a3e413941cab93d0
Hkm172Cse <azizulhakim000111@gmail.com>
2021-10-21 01:34:29 +0600

    added new method in Branch_B

--- hellow_2.java
+++ hellow_2.java
@@ -11,6 +11,10 @@ public class hellow2{
       public static void message(){
           System.out.println("This is a new Branch");
       }
+      
+      public static void display(){
+          System.out.println("this is another Method Form Branch B");
+      }
 
    }
 
