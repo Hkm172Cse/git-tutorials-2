8f70ded934d8a94f599e72be29533e49c194a61d
Hkm172Cse <azizulhakim000111@gmail.com>
2021-10-21 00:49:36 +0600

    remove some line from branch_B

--- hellow_2.java
+++ hellow_2.java
@@ -1,19 +1,5 @@
 public class hellow2{
     public static void main(String[] args){
-        display("azizul hakim");
-
-        System.out.println("Visual Studio Code is Difficult Awosome");
-        System.out.println("This is Check push");
         System.out.println("Now i am on Branch_B");
     }
-
-    public static void display(String name){
-        System.out.println("my name is "+ name);
-    }
-
-    public statci void sqr(int x){
-        return x*x;
-    }
-
-
 }
