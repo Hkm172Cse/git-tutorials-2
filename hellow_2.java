fbea809759047869ef6f7e51737a1d16e6298777
Hkm172Cse <azizulhakim000111@gmail.com>
2021-10-21 00:43:31 +0600

    delete some line on master branch

--- hellow_2.java
+++ hellow_2.java
@@ -1,15 +1,9 @@
 public class hellow2{
     public static void main(String[] args){
         display("azizul hakim");
-
-        System.out.println("Visual Studio Code is Difficult Awosome");
-        System.out.println("This is Check push");
         System.out.println("Now i am on Master Branch");
     }
 
-    public static void display(String name){
-        System.out.println("my name is "+ name);
-    }
-
+    
 
 }
