6dd09fcc111892d538e560d9b280824995eb86e6
Hkm172Cse <azizulhakim000111@gmail.com>
2021-10-20 11:13:23 +0600

    chainge some line on Branch_A

--- hellow_2.java
+++ hellow_2.java
@@ -2,8 +2,7 @@ public class hellow2{
     public static void main(String[] args){
         display("azizul hakim");
 
-        System.out.println("Visual Studio Code is Difficult Awosome");
-        System.out.println("This is Check push");
+      
     }
 
     public static void display(String name){
